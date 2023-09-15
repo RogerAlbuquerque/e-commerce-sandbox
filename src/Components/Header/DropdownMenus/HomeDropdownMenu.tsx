13d78cca7e4b483c50a7fe1723f256ed887c2a58
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2023-09-15 04:44:39 -0300

    feat: Structure css of header and add some images

---
+++ src/Components/Header/DropdownMenus/HomeDropdownMenu.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+export default function HomeDropdownMenu() {
+	return (
+		<article style={{display:'absolute'}}>
+			<h2>Choose section</h2>		
+			<section className='bg-blue-900 flex flex-wrap gap-x-4 absolute w-9/12'>
+				<div className='flex flex-col bg-red-600'>
+					<span>Imagem1 asdfasdfqfqwef</span>
+					<span>Text</span>
+				</div>
+				<div className='flex flex-col'>
+					<span>Imagem 2 asdfasdfqfqwef</span>
+					<span>Text</span>
+				</div>
+				<div className='flex flex-col'>
+					<span>Imagem 3 asdfasdfqfqwef</span>
+					<span>Text</span>
+				</div>
+				<div className='flex flex-col'>
+					<span>Imagem 4 asdfasdfqfqwef</span>
+					<span>Text</span>
+				</div>
+				<div className='flex flex-col'>
+					<span>Imagem 5 asdfasdfqfqwef</span>
+					<span>Text</span>
+				</div>
+				<div className='flex flex-col'>
+					<span>Imagem 6 asdfasdfqfqwef</span>
+					<span>Text</span>
+				</div>
+				<div className='flex flex-col'>
+					<span>Imagem 7 asdfasdfqfqwef</span>
+					<span>Text</span>
+				</div>
+				<div className='flex flex-col'>
+					<span>Imagem 8 asdfasdfqfqwef</span>
+					<span>Text</span>
+				</div>
+				
+				<div className='flex flex-col'>
+					<span>Imagem 9 asdfasdfqfqwef</span>
+					<span>Text</span>
+				</div>
+				
+				<div className='flex flex-col'>
+					<span>Imagem 10</span>
+					<span>Text</span>
+				</div>
+
+			</section>
+		</article>
+	);
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import Image from 'next/image';
-import phone from '../../public/telefone.png';
 import React from 'react';
+import HomeDropdownMenu from '../Components/Header/DropdownMenus/HomeDropdownMenu';
+import Image from 'next/image';
 
 
 export const metadata: Metadata = {
@@ -14,87 +14,105 @@ export default function RootLayout({children}:{children: React.ReactNode}) {
 	return (
 		<html lang="pt">
 			<body>
-				<header className='bg-red-600 text-slate-300'>
-					<article id='topHeader' className='flex justify-between'>
-						<section className='flex gap-2'>
-							<Image src={phone} width={16} alt='phone call logo'/>
-							Call: +55991234-5678
-						</section>
-						<section className='flex justify-between gap-6'>
-							<div>USD</div>
-							<div>Lang</div>
-							<div>Sign in / sign up</div>
-						</section>						
-					</article>
+				<div id='Container'>
+					<header className='bg-zinc-800 flex flex-col pl-40 pr-40 pt-2 text-slate-100'>
+						<article id='topHeader' className='flex justify-between pb-2'>
+							<section className='flex gap-2'>							
+								<Image 
+									src='/Imgs/telephone.png' 
+									width={20}
+									height={0}
+									alt="Picture of the author"
+								/>Call: +55991234-5678 
+							</section>
+							<section className='flex justify-between gap-6'>
+								<div>USD</div>
+								<div>Lang</div>
+								<div>Sign in / sign up</div>
+							</section>						
+						</article>
+						<hr className='border-neutral-600'/>
+						<article id='centerHeader' className='flex justify-between pt-10 pb-10'>
+							<section>
+								<Image
+									src="/Imgs/logo.png"
+									width={100}
+									height={0}
+									alt="Picture of the author"
+								/>
+							</section>
+							<section>Search Bar</section>
+							<section className='flex justify-between gap-6'>
+								<div>Compare</div>
+								<div>Wishlist</div>
+								<div>Cart</div>
+							</section>
+						</article>					
+					</header>	
 
-					<article id='centerHeader' className='flex justify-between'>
-						<section>Logo</section>
-						<section>Search Bar</section>
-						<section className='flex justify-between gap-6'>
-							<div>Compare</div>
-							<div>Wishlist</div>
-							<div>Cart</div>
-						</section>
-					</article>
+					<nav className='text-black pl-40 pr-40 pt-2' >
+						<article id='fixedHeader' className='flex justify-between font-medium'>
+							<section>Browse Categories</section>
+							<section>
+								<ul className='flex gap-20 '>
+									<li className=''>
+										<div>Home</div>
+										<div className='hidden'><HomeDropdownMenu /></div>
+									</li>
+									<li>Shop</li>
+									<li>Product</li>
+									<li>Pages</li>
+									<li>Blog</li>
+									<li>Elements</li>
+								</ul>
+							</section>
+							<section>Clearence</section>
+						</article>
+					</nav>
 
-					<article id='fixedHeader' className='flex justify-between'>
-						<section>HAMBURGUER MENU</section>
-						<section>
-							<ul className='flex gap-10 font-bold'>
-								<li>Home</li>
-								<li>Shop</li>
-								<li>Product</li>
-								<li>Pages</li>
-								<li>Blog</li>
-								<li>Elements</li>
+					<main className='bg-blue-600 text-slate-50  pl-40 pr-40 pt-2'>
+						{children}
+					</main>
+					
+					<footer className='bg-green-600 text-slate-300 flex gap-24  pl-40 pr-40 pt-2'>
+						<section id='footer1' className='w-60 flex flex-col gap-2'>
+							<div>LOGO</div>
+							<div>Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.</div>
+							<div>Contact</div>
+						</section>
+						<section id='footer2'>
+							<div>Useful Links</div>
+							<ul>
+								<li>About Molla</li>
+								<li>Our Services</li>
+								<li>How to shop on Molla</li>
+								<li>FAQ						</li>
+								<li>Contact us</li>
 							</ul>
 						</section>
-						<section>Clearence</section>
-					</article>
-				</header>	
-
-				<main className='bg-blue-600 text-slate-50'>
-					{children}
-				</main>
-				
-				<footer className='bg-green-600 text-slate-300 flex gap-24'>
-					<section id='footer1' className='w-60 flex flex-col gap-2'>
-						<div>LOGO</div>
-						<div>Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.</div>
-						<div>Contact</div>
-					</section>
-					<section id='footer2'>
-						<div>Useful Links</div>
-						<ul>
-							<li>About Molla</li>
-							<li>Our Services</li>
-							<li>How to shop on Molla</li>
-							<li>FAQ						</li>
-							<li>Contact us</li>
-						</ul>
-					</section>
-					<section id='footer3'>
-						<div>Customer Services</div>
-						<ul>
-							<li>Money-back guarantee!</li>
-							<li>Payment Methods</li>
-							<li>Returns</li>
-							<li>Shipping</li>
-							<li>Terms and conditions</li>
-							<li>Privacy Policy</li>
-						</ul>
-					</section>
-					<section id='footer4'>
-						<div>My account</div>
-						<ul>
-							<li>Sign In</li>
-							<li>View Cart</li>
-							<li>My Wishlist</li>
-							<li>Track My Order</li>
-							<li>Help</li>
-						</ul>
-					</section>
-				</footer>			
+						<section id='footer3'>
+							<div>Customer Services</div>
+							<ul>
+								<li>Money-back guarantee!</li>
+								<li>Payment Methods</li>
+								<li>Returns</li>
+								<li>Shipping</li>
+								<li>Terms and conditions</li>
+								<li>Privacy Policy</li>
+							</ul>
+						</section>
+						<section id='footer4'>
+							<div>My account</div>
+							<ul>
+								<li>Sign In</li>
+								<li>View Cart</li>
+								<li>My Wishlist</li>
+								<li>Track My Order</li>
+								<li>Help</li>
+							</ul>
+						</section>
+					</footer>
+				</div>			
 			</body>
 		</html>
 	);
