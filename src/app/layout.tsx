075141eb229cf69a9afc09c41f15d8e6395fbeb6
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2023-09-15 16:59:08 -0300

    feat: Search bar on header stylized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import React from 'react';
 import HomeDropdownMenu from '../Components/Header/DropdownMenus/HomeDropdownMenu';
 import Image from 'next/image';
+import SearchIcon from '../Components/Header/SearchIconButton';
 
 
 export const metadata: Metadata = {
@@ -15,14 +16,15 @@ export default function RootLayout({children}:{children: React.ReactNode}) {
 		<html lang="pt">
 			<body>
 				<div id='Container'>
-					<header className='bg-zinc-800 flex flex-col pl-40 pr-40 pt-2 text-slate-100'>
+					<header className='bg-zinc-800 flex flex-col pl-96 pr-96 pt-2 text-slate-100'>
 						<article id='topHeader' className='flex justify-between pb-2'>
-							<section className='flex gap-2'>							
+							<section className='flex gap-2 items-center'>							
 								<Image 
 									src='/Imgs/telephone.png' 
-									width={20}
-									height={0}
-									alt="Picture of the author"
+									width={14}
+									height={14}
+									alt="icon of a telephone to show contact number"
+									style={{height:'14px'}}
 								/>Call: +55991234-5678 
 							</section>
 							<section className='flex justify-between gap-6'>
@@ -32,16 +34,23 @@ export default function RootLayout({children}:{children: React.ReactNode}) {
 							</section>						
 						</article>
 						<hr className='border-neutral-600'/>
-						<article id='centerHeader' className='flex justify-between pt-10 pb-10'>
+						<article id='centerHeader' className='flex justify-between items-center pt-6 pb-6'>
 							<section>
 								<Image
 									src="/Imgs/logo.png"
 									width={100}
 									height={0}
-									alt="Picture of the author"
+									alt="website logo"
 								/>
 							</section>
-							<section>Search Bar</section>
+							<section>
+								<div className='flex items-center bg-white rounded-full pl-4'>
+									<i>
+										<SearchIcon />
+									</i>
+									<input type="search" name="SearchProducts" id="SearchProducts" className='text-black p-3 ml-2 rounded-full' style={{width:'30vw'}}/>
+								</div>								
+							</section>
 							<section className='flex justify-between gap-6'>
 								<div>Compare</div>
 								<div>Wishlist</div>
@@ -50,31 +59,43 @@ export default function RootLayout({children}:{children: React.ReactNode}) {
 						</article>					
 					</header>	
 
-					<nav className='text-black pl-40 pr-40 pt-2' >
+					<nav className='text-black pl-96 pr-96 pt-2' >
 						<article id='fixedHeader' className='flex justify-between font-medium'>
 							<section>Browse Categories</section>
 							<section>
 								<ul className='flex gap-20 '>
 									<li className=''>
 										<div>Home</div>
-										<div className='hidden'><HomeDropdownMenu /></div>
+										<div className='hidden'>
+											<HomeDropdownMenu />
+										</div>
 									</li>
-									<li>Shop</li>
-									<li>Product</li>
-									<li>Pages</li>
-									<li>Blog</li>
-									<li>Elements</li>
+									<li>
+										Shop										
+									</li>
+									<li>
+										Product										
+									</li>
+									<li>
+										Pages										
+									</li>
+									<li>
+										Blog										
+									</li>
+									<li>
+										Elements										
+									</li>
 								</ul>
 							</section>
 							<section>Clearence</section>
 						</article>
 					</nav>
 
-					<main className='bg-blue-600 text-slate-50  pl-40 pr-40 pt-2'>
+					<main className='bg-blue-600 text-slate-50  pl-96 pr-96 pt-2'>
 						{children}
 					</main>
 					
-					<footer className='bg-green-600 text-slate-300 flex gap-24  pl-40 pr-40 pt-2'>
+					<footer className='bg-green-600 text-slate-300 flex gap-24  pl-96 pr-96 pt-2'>
 						<section id='footer1' className='w-60 flex flex-col gap-2'>
 							<div>LOGO</div>
 							<div>Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue, eu vulputate magna eros eu erat.</div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 export default function Index() {
 	return (
-		<h1 className='font-bold'>HELLO WORLD</h1>
+		<>
+			<h1 className='font-bold'>HELLO WORLD</h1>
+		</>
+		
 	);
 }
