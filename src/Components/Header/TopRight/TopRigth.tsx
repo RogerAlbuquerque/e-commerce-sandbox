68444baf46669679e308512767e878601bffe836
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2023-09-17 11:04:32 -0300

    feat:Add dropdown menu to header

--- src/Components/Header/TopRight/TopRigth.tsx
+++ src/Components/Header/TopRight/TopRigth.tsx
@@ -1,39 +1,42 @@
 'use client';
 import React from 'react';
 export default function TopRight() {
-	function DropDownMenuShow(menuId:string){
-		const menu = document.getElementById(menuId)?.style;			
-		menu!.display = 'flex';
-	}
-	function DropDownMenuHidden(menuId:string){
-		const menu = document.getElementById(menuId)?.style;
-		setTimeout(function() {
-		menu!.display = 'none';
-		}, 2000);
-	}
+	// function showMenu(menuId:string){
+	// 	const menu = document.getElementById(menuId)?.style;
+	// 	menu!.display = 'flex';
+	// }
+
+	// function hideMenu(menuId:string){
+	// 	const menu = document.getElementById(menuId)?.style;
+	// 	menu!.display ='none';
+	// }
 	return (
-		<section className='flex justify-between gap-6'>
-			<div onMouseEnter={() => DropDownMenuShow('moneyType')} onMouseOut={() => DropDownMenuHidden('moneyType')}>
-				<a href='#' >
-					USD &#8595;
-				</a>
-				<ul id='moneyType' className='bg-white text-neutral-400 flex flex-col p-2 mt-2 absolute hidden'>
-					<a href='#'>USD</a>
-					<a href='#'>EUR</a>
-				</ul>
+		<section className='flex justify-between gap-6'>			
+			<div className="group">
+				<a href="#">USD &#8595;</a>
+				<div className="hidden absolute w-32 bg-white group-hover:block">
+					<ul  id='money' className='bg-white text-neutral-400 flex flex-col p-2 absolute '>
+						<a href='#'>USD</a>
+						<a href='#'>EUR</a>
+					</ul>
+				</div>
 			</div>
-			<div onMouseEnter={() => DropDownMenuShow('langMenu')} onMouseOut={() => DropDownMenuHidden('langMenu')}>
-				<a href='#' >English &#8595;</a>
-				<ul id='langMenu' className='bg-white text-neutral-400 flex flex-col p-2 mt-2 absolute hidden'>
-					<a href='#'>English</a>
-					<a href='#'>French</a>
-					<a href='#'>Portuguese</a>
-				</ul>
+
+			<div className="group">
+				<a href="#">English &#8595;</a>
+				<div className="hidden absolute w-32 bg-white group-hover:block">
+					<ul  id='money' className='bg-white text-neutral-400 flex flex-col p-2  absolute '>
+						<a href='#'>English</a>
+						<a href='#'>French</a>
+						<a href='#'>Portuguese</a>
+					</ul>
+				</div>
 			</div>
+
 			<div>
 				<a href='#'>Sign in / sign up</a>
-			</div>
-			
+			</div>	
+
 		</section>
 	);
 }
