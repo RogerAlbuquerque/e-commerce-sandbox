db847720a093cd07cf1037889678a7fbc0ce28ed
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2023-11-11 22:56:44 -0300

    add submenus on navigation menus

--- src/Components/Header/DropdownMenus/Blog.tsx
+++ src/Components/Header/DropdownMenus/Blog.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 export default function BlogsDropdownMenu() {
 	return (
-		<article className='absolute drop-shadow-lg bg-white p-4 flex border-8' style={{top:'2.47rem'}}>
+		<article className='absolute drop-shadow-lg bg-white p-4 flex' style={{top:'2.47rem'}}>
 			<ul>
 				<li className='font-normal' style={{ color: 'rgb(168, 167, 167)' }}><a href='#'>Classic</a></li>
 				<li className='pt-3 font-normal' style={{ color: 'rgb(168, 167, 167)' }}><a href='#'>Listing</a></li>
--- src/Components/Header/DropdownMenus/Pages.tsx
+++ src/Components/Header/DropdownMenus/Pages.tsx
@@ -4,11 +4,23 @@ export default function PagesDropdownMenu() {
 	return (
 		<article className='absolute drop-shadow-lg bg-white p-4 flex'>
 			<ul>
-				<li className='font-normal' style={{ color: 'rgb(168, 167, 167)' }}>
+				<li className='font-normal relative' style={{ color: 'rgb(168, 167, 167)' }}>
 					<a href='#'>About</a>
+					<div className='absolute w-40  p-4 bg-white hidden' style={{left:'7em', top:'-2em'}}>
+						<ul className=''>
+							<li className='pb-2'><a href='#'>About 01</a></li>
+							<li><a href='#'>About 02</a></li>
+						</ul>
+					</div>
 				</li>
-				<li className='pt-3 font-normal' style={{ color: 'rgb(168, 167, 167)' }}>
+				<li className='pt-3 font-normal relative' style={{ color: 'rgb(168, 167, 167)' }}>
 					<a href='#'>Contact</a>
+					<div className='absolute w-40  p-4 bg-white hidden' style={{left:'7em', top:'-2em'}}>
+						<ul className=''>
+							<li className='pb-2'><a href='#'>Contact 01</a></li>
+							<li><a href='#'>Contact 02</a></li>
+						</ul>
+					</div>
 				</li>
 				
 				<li className='pt-3 font-normal' style={{ color: 'rgb(168, 167, 167)' }}><a href='#'>Login</a></li>
