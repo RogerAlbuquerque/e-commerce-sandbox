88c115e9570f65f29f1f5a1507e3f960003d0738
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2024-02-03 10:10:10 -0300

    style: stylized page products likns on top OK

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,47 +1,65 @@
 import React from 'react';
+import { MdArrowForwardIos } from 'react-icons/md';
 export default function Product() {
 	return (
 		<main className='flex justify-center'>
-			<div className='customContainer'>
-				<div className='flex justify-between'>
-					<ul className='flex gap-8'>
+			<article className='customContainer'>
+				<section className='flex justify-between'>
+					<ul className='flex gap-2 items-center text-stone-500'>
 						<li><a href="#">Home</a></li>
+						<MdArrowForwardIos className='text-xs'/>
 						<li><a href="#">Product</a></li>
-						<li><a href="#">Default</a></li>
+						<MdArrowForwardIos className='text-xs'/>
+						<li><a href="#" className='text-black'>Default</a></li>
 					</ul>
 
 					<ul className='flex gap-4'>
-						<li><a href="#">Prev</a></li>
-						<li><a href="#">Next</a></li>
+						<li>
+							<a href="#" className='flex items-center gap-1 text-stone-500'>
+								<MdArrowForwardIos className='text-lg rotate-180'/> Prev
+							</a>
+						</li>
+						<li>
+							<a href="#" className='flex items-center gap-1 text-stone-500'>
+							Next <MdArrowForwardIos className='text-lg'/>
+							</a>
+						</li>
 					</ul>
-				</div>
+				</section>
 
 				
 				
-				<div>
-					<div>
+				<article className='flex justify-between border border-red-600'>
+					<div className='flex flex-1 gap-4 border border-green-600'>
 						<div>carrosel fotos</div>
 						<div>foto maior</div>
 					</div>
 					
-					<div>
+					<div className='flex flex-1 gap-4 border border-green-600'>
 						INFORMAÇÕES DO PRODUTO
 					</div>
-				</div>
+				</article>
 
 
 
-				<div>
-					<div>INFORMAÇÕES AVANÇADAS</div>
-					<div>INFORMAÇÕES DETALHADAS DO PRODUTO</div>
-				</div>
+				<article>					
+					<ul className='flex justify-center gap-4 border'>
+						<li><a href='#'>Description</a></li>
+						<li><a href='#'>Additional Information</a></li>
+						<li><a href='#'>Shipping & Returns</a></li>
+						<li><a href='#'>Reviews</a></li>
+					</ul>
+					<div className='border p-4'>
+						INFORMAÇÕES DETALHADAS DO PRODUTO
+					</div>
+				</article>
 
 
-				<div>
-					<div>You May Also Like</div>
+				<article>
+					<div className='text-center'>You May Also Like</div>
 					<div>CARROSEL DE PRODUTOS RELACIONADOS</div>
-				</div>
-			</div>
+				</article>
+			</article>
 
 		</main>
 		
