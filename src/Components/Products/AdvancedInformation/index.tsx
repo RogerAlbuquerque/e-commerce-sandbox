68f2d35a906076daa0ea4585dbfe8a5472e08d93
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2024-02-08 07:12:20 -0300

    refactor:clean unused tailwind classes

--- src/Components/Products/AdvancedInformation/index.tsx
+++ src/Components/Products/AdvancedInformation/index.tsx
@@ -1,7 +1,7 @@
 
 import CardProduct from '@/Components/Home/CardProduct';
 import React from 'react';
-
+import './style.css';
 export default function AdvancedInformation(){
 	return(
 		<article>
@@ -20,22 +20,57 @@ export default function AdvancedInformation(){
 				</li>
 			</ul>
 
-			<div className='p-6 border flex flex-col gap-2 leading-6'>
-				<h1 className='font-normal text-lg'>Product Information</h1>
-				<p className='text-stone-500'>						
-					Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus.
-				</p>
-
-				<ul className='list-disc pl-5 text-stone-700'>
-					<li>Nunc nec porttitor turpis. In eu risus enim. In vitae mollis elit.</li>
-					<li>Vivamus finibus vel mauris ut vehicula.</li>
-					<li>Nullam a magna porttitor, dictum risus nec, faucibus sapien.</li>
-				</ul>
-					
-				<p className=' text-stone-600'>
+			<article id='advancedInfos' className='p-6 border flex flex-col gap-2 '>
+				
+				<div id='Description' className='leading-7'>
+					<h1>Product Information</h1>
+					<p >						
 						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus.
-				</p>
-			</div>
+					</p>
+
+					<ul>
+						<li>Nunc nec porttitor turpis. In eu risus enim. In vitae mollis elit.</li>
+						<li>Vivamus finibus vel mauris ut vehicula.</li>
+						<li>Nullam a magna porttitor, dictum risus nec, faucibus sapien.</li>
+					</ul>
+						
+					<p>
+							Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci. Aenean dignissim pellentesque felis. Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus.
+					</p>
+				</div>
+
+				<div id='Additional' className='hidden flex flex-col gap-2  leading-7'>				
+					<h2>Information</h2>
+					<p>
+						Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna viverra non, semper suscipit, posuere a, pede. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci.
+					</p>
+
+					<h2>Fabric & care</h2>
+					<ul>
+						<li>Faux suede fabric</li>
+						<li>Gold tone metal hoop handles.</li>
+						<li>RI branding</li>
+						<li>Snake print trim interior</li>
+						<li>Adjustable cross body strap</li>
+						<li>Height: 31cm; Width: 32cm; Depth: 12cm; Handle Drop: 61cm</li>
+					</ul>
+				
+					<h2>Size</h2>
+					<p>one size</p>
+				</div>
+
+				<div id='Shipping & Return' className='hidden flex flex-col gap-2 leading-7'>
+					<h2>Delivery & returns</h2>
+					<p>
+						We deliver to over 100 countries around the world. For full details of the delivery options we offer, please view our Delivery information
+						We hope you’ll love every purchase, but if you ever need to return an item you can do so within a month of receipt. For full details of how to make a return, please view our Returns information
+					</p>
+				</div>
+
+				<div id='Reviews' className='hidden flex flex-col gap-2 leading-7'>
+					Reviews
+				</div>
+			</article>
 
 
 			<article className='mb-8'>
