10fc825bb6fe33e8366a98ab186e3dd9416e55dc
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-02-13 22:34:18 -0300

    feat-backend: Placed comments for routes that need to have in project

--- back/src/server.ts
+++ back/src/server.ts
@@ -16,4 +16,82 @@ app.use((req,res,next) =>{
 
 app.use(express.json());
 
+
+/*ROUTES -----------------------------------
+*
+* Create products
+*
+*
+* Create Categories
+*
+*
+* Update product infos
+*
+*
+* Show some number (like 10 or 20) of products
+*
+*
+* Show/Search list of products filtered
+*
+*
+* Show product by id/name
+*
+*
+* Show list of products by id
+*
+*
+* Show list of products by category
+*
+*
+*
+*
+* Sell list of products if have in stock (products in cart)
+*
+*
+*
+*
+* CLIENTS""""""""""""""""""""""""""""""""""""""""""""""
+*
+* Create/Register Client
+*
+*
+* Update client info
+*
+*
+* Update Client infos*
+*
+*
+*
+*
+*
+*
+* CLIENTS END """"""""""""""""""""""""""""""""""""""""""
+*
+*
+*
+*
+
+*
+*
+*
+*
+*
+*
+*
+* AUTENTICATION---------------------------------------------
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* */
+
+
+
 app.listen(port,() => { console.log(`Server is running on port ${8000}`)});
