cbad5e24005ee861472a1006c5212f4678d99346
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2024-04-25 18:48:58 -0300

    a

--- src/Components/Home/TrendingProducts/listMenu/index.tsx
+++ src/Components/Home/TrendingProducts/listMenu/index.tsx
@@ -11,11 +11,11 @@ export default function ListMenu(){
 	}
 	return(
 		<ul className='flex max-sm:justify-center gap-6 flex-wrap text-stone-800'>
-			<li className={`${listMenuStatus[0] && 'border-b-2 border-amber-400 text-amber-500 transition delay-100 px-2'}`}><button onClick={() => handleMenuListStatus(0)} >ALL</button></li>
-			<li className={`${listMenuStatus[1] && 'border-b-2 border-amber-400 text-amber-500 transition delay-100 px-2'}`}><button onClick={() => handleMenuListStatus(1)} >ACCESSORIES</button></li>
-			<li className={`${listMenuStatus[2] && 'border-b-2 border-amber-400 text-amber-500 transition delay-100 px-2'}`}><button onClick={() => handleMenuListStatus(2)} >CAMERAS & CAMCORDERS</button></li>
-			<li className={`${listMenuStatus[3] && 'border-b-2 border-amber-400 text-amber-500 transition delay-100 px-2'}`}><button onClick={() => handleMenuListStatus(3)} >COMPUTERS & TABLETS</button></li>
-			<li className={`${listMenuStatus[4] && 'border-b-2 border-amber-400 text-amber-500 transition delay-100 px-2'}`}><button onClick={() => handleMenuListStatus(4)} >ENTERTAINMENT</button></li>
+			<li className={`${listMenuStatus[0] && 'border-b-2 border-amber-400 text-amber-500 transition delay-100 px-2'}`}><button onClick={() => handleMenuListStatus(0)}>ALL</button></li>
+			<li className={`${listMenuStatus[1] && 'border-b-2 border-amber-400 text-amber-500 transition delay-100 px-2'}`}><button onClick={() => handleMenuListStatus(1)}>ACCESSORIES</button></li>
+			<li className={`${listMenuStatus[2] && 'border-b-2 border-amber-400 text-amber-500 transition delay-100 px-2'}`}><button onClick={() => handleMenuListStatus(2)}>CAMERAS & CAMCORDERS</button></li>
+			<li className={`${listMenuStatus[3] && 'border-b-2 border-amber-400 text-amber-500 transition delay-100 px-2'}`}><button onClick={() => handleMenuListStatus(3)}>COMPUTERS & TABLETS</button></li>
+			<li className={`${listMenuStatus[4] && 'border-b-2 border-amber-400 text-amber-500 transition delay-100 px-2'}`}><button onClick={() => handleMenuListStatus(4)}>ENTERTAINMENT</button></li>
 		</ul>			
 	);
 }
