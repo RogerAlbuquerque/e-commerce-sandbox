5cc076f347861f27de26f47925e7c4f34c7b94bf
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2024-12-24 11:32:51 -0300

    change some data on product list

--- frontend/src/utils/hardListOfProducts.ts
+++ frontend/src/utils/hardListOfProducts.ts
@@ -2,180 +2,178 @@ import { typeListProducts } from "@/@types/listProducts";
 import { computeFromManifest } from "next/dist/build/utils";
 
 export const productList: typeListProducts[] = [
-	{
-		productId: 1,
-		name: 'test1',
-		price: 10.25,
-		color: ['#ff0000'],
-		productCategories: ['Accessories', 'Smartwatches'],
-		imagesPath: {
-			featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
-			secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg' //This need to be an array 
-		},
-		productState: {
-			sale: true,
-			newProduct: true,
-			topProduct: true,
-		},
-		stars: 5
-	},
-	{
-		productId: 2,
-		name: 'test2',
-		price: 10.25,
-		color: ['#ff0000'],
-		productCategories: ['Accessories', 'Smartwatches'],
-		imagesPath: {
-			featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
-			secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg' //This need to be an array 
-		},
-		productState: {
-			sale: true,
-			newProduct: true,
-			topProduct: true,
-		},
-		stars: 4
-	},
-	{
-		productId: 3,
-		name: 'test3',
-		price: 10.25,
-		color: ['#ff0000'],
-		productCategories: ['Accessories', 'Smartwatches'],
-		imagesPath: {
-			featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
-			secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg' //This need to be an array 
-		},
-		productState: {
-			sale: true,
-			newProduct: true,
-			topProduct: true,
-		},
-		stars: 5
-	},
-	{
-		productId: 4,
-		name: 'test4',
-		price: 10.25,
-		color: ['#ff0000'],
-		productCategories: ['Accessories', 'Smartwatches'],
-		imagesPath: {
-			featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
-			secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg' //This need to be an array 
-		},
-		productState: {
-			sale: true,
-			newProduct: true,
-			topProduct: true,
-		},
-		stars: 1
-	},
-	{
-		productId: 5,
-		name: 'test5',
-		price: 10.25,
-		color: ['#ff0000'],
-		productCategories: ['Accessories', 'Smartwatches'],
-		imagesPath: {
-			featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
-			secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg' //This need to be an array 
-		},
-		productState: {
-			sale: true,
-			newProduct: true,
-			topProduct: true,
-		},
-		stars: 2
-	},
-	{
-		productId: 6,
-		name: 'test6',
-		price: 10.25,
-		color: ['#ff0000'],
-		productCategories: ['Accessories', 'Smartwatches'],
-		imagesPath: {
-			featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
-			secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg' //This need to be an array 
-		},
-		productState: {
-			sale: true,
-			newProduct: true,
-			topProduct: true,
-		},
-		stars: 5
-	},
-	{
-		productId: 7,
-		name: 'test7',
-		price: 10.25,
-		color: ['#ff0000'],
-		productCategories: ['Accessories', 'Smartwatches'],
-		imagesPath: {
-			featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
-			secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg' //This need to be an array 
-		},
-		productState: {
-			sale: true,
-			newProduct: true,
-			topProduct: true,
-		},
-		stars: 5
-	},
-	{
-		productId: 8,
-		name: 'test8',
-		price: 10.25,
-		color: ['#ff0000'],
-		productCategories: ['Accessories', 'Smartwatches'],
-		imagesPath: {
-			featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
-			secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg' //This need to be an array 
-		},
-		productState: {
-			sale: true,
-			newProduct: true,
-			topProduct: true,
-		},
-		stars: 3
-	},
-	{
-		productId: 9,
-		name: 'test9',
-		price: 10.25,
-		color: ['#ff0000'],
-		productCategories: ['Accessories', 'Smartwatches'],
-		imagesPath: {
-			featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
-			secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg' //This need to be an array 
-		},
-		productState: {
-			sale: true,
-			newProduct: true,
-			topProduct: true,
-		},
-		stars: 3
-	},
-	{
-		productId: 10,
-		name: 'test10',
-		price: 10.25,
-		color: ['#ff0000'],
-		productCategories: ['Accessories', 'Smartwatches'],
-		imagesPath: {
-			featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
-			secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg' //This need to be an array 
-		},
-		productState: {
-			sale: true,
-			newProduct: true,
-			topProduct: true,
-		},
-		stars: 3
-	},
 
+    {
+        productId: 1,
+        name: 'Macbook',
+        price: 153.99,
+        color: ['#1a1a1a', '#ff5733', '#33ff57'],
+        productCategories: ['Accessories', 'Smartwatches'],
+        imagesPath: {
+            featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
+            secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg'
+        },
+        productState: {
+            sale: true,
+            newProduct: true,
+            topProduct: true,
+        },
+        stars: 5
+    },
+    {
+        productId: 2,
+        name: 'Macbook',
+        price: 85.49,
+        color: ['#0055ff', '#dd9900'],
+        productCategories: ['Accessories', 'Smartwatches'],
+        imagesPath: {
+            featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
+            secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg'
+        },
+        productState: {
+            sale: true,
+            newProduct: true,
+            topProduct: true,
+        },
+        stars: 4
+    },
+    {
+        productId: 3,
+        name: 'Macbook',
+        price: 132.75,
+        color: ['#ff0000', '#00ff00', '#0000ff', '#ff00ff'],
+        productCategories: ['Accessories', 'Smartwatches'],
+        imagesPath: {
+            featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
+            secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg'
+        },
+        productState: {
+            sale: true,
+            newProduct: true,
+            topProduct: true,
+        },
+        stars: 5
+    },
+    {
+        productId: 4,
+        name: 'Macbook',
+        price: 49.99,
+        color: ['#abc123', '#321cba'],
+        productCategories: ['Accessories', 'Smartwatches'],
+        imagesPath: {
+            featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
+            secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg'
+        },
+        productState: {
+            sale: true,
+            newProduct: true,
+            topProduct: true,
+        },
+        stars: 1
+    },
+    {
+        productId: 5,
+        name: 'Macbook',
+        price: 78.90,
+        color: ['#ffaa00', '#aabbcc', '#00aaff'],
+        productCategories: ['Accessories', 'Smartwatches'],
+        imagesPath: {
+            featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
+            secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg'
+        },
+        productState: {
+            sale: true,
+            newProduct: true,
+            topProduct: true,
+        },
+        stars: 2
+    },
+    {
+        productId: 6,
+        name: 'Macbook',
+        price: 140.00,
+        color: ['#ff007f', '#7fff00', '#007fff', '#333333', '#444444'],
+        productCategories: ['Accessories', 'Smartwatches'],
+        imagesPath: {
+            featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
+            secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg'
+        },
+        productState: {
+            sale: true,
+            newProduct: true,
+            topProduct: true,
+        },
+        stars: 5
+    },
+    {
+        productId: 7,
+        name: 'Macbook',
+        price: 120.75,
+        color: ['#001100', '#110022', '#223300'],
+        productCategories: ['Accessories', 'Smartwatches'],
+        imagesPath: {
+            featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
+            secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg'
+        },
+        productState: {
+            sale: true,
+            newProduct: true,
+            topProduct: true,
+        },
+        stars: 5
+    },
+    {
+        productId: 8,
+        name: 'Macbook',
+        price: 60.50,
+        color: ['#123456', '#654321', '#abcdef'],
+        productCategories: ['Accessories', 'Smartwatches'],
+        imagesPath: {
+            featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
+            secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg'
+        },
+        productState: {
+            sale: true,
+            newProduct: true,
+            topProduct: true,
+        },
+        stars: 3
+    },
+    {
+        productId: 9,
+        name: 'Macbook',
+        price: 99.99,
+        color: ['#001234', '#456789'],
+        productCategories: ['Accessories', 'Smartwatches'],
+        imagesPath: {
+            featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
+            secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg'
+        },
+        productState: {
+            sale: true,
+            newProduct: true,
+            topProduct: true,
+        },
+        stars: 3
+    },
+    {
+        productId: 10,
+        name: 'Macbook',
+        price: 105.45,
+        color: ['#f1f1f1', '#0f0f0f', '#fa8072'],
+        productCategories: ['Accessories', 'Smartwatches'],
+        imagesPath: {
+            featuredImagePath: '/Imgs/Main/productsImages/notebook/macbook.jpg',
+            secondaryImagesPath: '/Imgs/Main/productsImages/notebook/macbook2.jpg'
+        },
+        productState: {
+            sale: true,
+            newProduct: true,
+            topProduct: true,
+        },
+        stars: 3
+    }
 ]
-
-
 
 export const productList2: typeListProducts[] = [
 	{
