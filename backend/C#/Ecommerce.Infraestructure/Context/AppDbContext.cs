2319a1fb7d159756baee842063394702d28e8bac
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2025-01-04 15:40:35 -0300

    feat:Configured DbContext on infraestructure project and connectionString on Api project

---
+++ backend/C#/Ecommerce.Infraestructure/Context/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
+namespace Ecommerce.Infrastructure.Context;
+
+public class AppDbContext(IConfiguration configuration, DbContextOptions options) : DbContext(options)
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Configuring an especific error on Ecommerce.Exception project and call here if connection string is null 
+        var connectionString = _configuration.GetConnectionString("Mysql") ?? throw new ArgumentException(null);
+
+        optionsBuilder.UseMySQL(connectionString);
+    }
+}
