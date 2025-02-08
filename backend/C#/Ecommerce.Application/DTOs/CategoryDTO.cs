25b450cbb5665633ae5c7b6cb50ddea2db1810d7
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2025-02-08 18:24:38 -0300

    Feat: Added some data annotations for DTO

--- backend/C#/Ecommerce.Application/DTOs/CategoryDTO.cs
+++ backend/C#/Ecommerce.Application/DTOs/CategoryDTO.cs
@@ -1,9 +1,12 @@
-﻿using Ecommerce.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Ecommerce.Application.DTOs;
 
 public class CategoryDTO
 {
     public Guid CategoryId { get; private set; }
+    [Required(ErrorMessage = "Enter the category name")]
+    [MinLength(3)]
+    [MaxLength(100)]
     public string Name { get; private set; } = string.Empty;
 }
--- backend/C#/Ecommerce.Application/DTOs/ProductDTO.cs
+++ backend/C#/Ecommerce.Application/DTOs/ProductDTO.cs
@@ -1,17 +1,33 @@
-﻿using Ecommerce.Domain.Entities;
-using Ecommerce.Domain.Enums;
+﻿using Ecommerce.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ecommerce.Application.DTOs;
 
 public class ProductDTO
 {
     public Guid ProductId { get; private set; } = Guid.NewGuid();
+
+    [Required(ErrorMessage = "Name is required")]
+    [MinLength(3)]
+    [MaxLength(100)]
     public string Name { get; private set; } = string.Empty;
+
+    [Required(ErrorMessage = "Enter the price")]
+    [Column(TypeName = "decimal(18,2)")]
+    [DisplayFormat(DataFormatString = "{0:C2}")]
+    [DataType(DataType.Currency)]
     public decimal Price { get; private set; }
+
     public IList<string> HexColor { get; private set; } = [];
+
     public SizeEnum Size { get; private set; }
+
+    [Range(0, 5)]
     public byte Stars { get; private set; }
+
     public string FeaturedImagePath { get; private set; } = string.Empty;
+
     public IList<string>? SecondaryImagesPath { get; private set; } = [];
 
     public ProductStateDTO? ProductState { get; set; }
