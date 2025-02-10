34b92923e0a850e142c5635043cdfc186583c55f
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2025-02-10 09:37:34 -0300

    Feat: Created mapping to AutoMapper work well

---
+++ backend/C#/Ecommerce.Application/DTOMappings/DTOMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Ecommerce.Application.DTOs;
+using Ecommerce.Domain.Entities;
+namespace Ecommerce.Application.DTOMappings;
+
+public class DTOMappingProfile : Profile
+{
+    DTOMappingProfile()
+    {
+        CreateMap<Category, CategoryDTO>().ReverseMap();
+        CreateMap<Product, ProductDTO>().ReverseMap();
+    }
+}
