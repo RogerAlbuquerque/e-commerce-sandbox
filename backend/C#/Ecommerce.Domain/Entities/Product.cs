6227ccb87936edb6a02a01d717c8dde3c2d56ce0
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2025-02-13 19:49:45 -0300

    Fix: Wrong property in products entity

--- backend/C#/Ecommerce.Domain/Entities/Product.cs
+++ backend/C#/Ecommerce.Domain/Entities/Product.cs
@@ -9,8 +9,8 @@ public sealed class Product
     public string Name { get; private set; } = string.Empty;
     public decimal Price { get; private set; }
     public IList<string> HexColor { get; private set; } = [];
-    public SizeEnum Size{ get; private set; }      
-    public byte Stars {  get; private set; }         
+    public String Size{ get; private set; }      
+    public byte Stars {  get; private set; }    
     public string FeaturedImagePath { get; private set; } = string.Empty;
     public IList<string>? SecondaryImagesPath { get; private set; } = [];
     public DateTime RegisterDate { get; private set; }
