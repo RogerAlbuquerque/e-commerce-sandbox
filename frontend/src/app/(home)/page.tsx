fb084d6c414798276b63dec98b7a76cf98682062
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2025-02-15 11:30:38 -0300

    feat: Created a call to backend to test if work good with frontend

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -11,32 +11,33 @@ import MainShowedProducts from './Components/MainShowedProducts';
 import { typeListProducts } from '@/@types/listProducts';
 import { productList} from '@/utils/hardListOfProducts';
 
-export async function getServerSideProps() {
-  const res = await fetch('https://api.exemplo.com/dados');
-  const data = await res.json();
-
-  return {
-    props: {
-      dados: data, // será passado como props para o componente
-    },
-  };
-}
 
 
 export default function Index() {
 	// ------------------------------------------------ JUST FOR FIRST TESTS ------------------------------------------------
-	// const [listProducts0] = useState<typeListProducts[]>(productList);
+	const [listProducts0, setListProducts0] = useState<typeListProducts[]>();
 	// ------------------------------------------------ JUST FOR FIRST TESTS ------------------------------------------------
 	const [listProducts] = useState<typeListProducts[]>(productList);
 	const [dealsProducts] = useState<typeListProducts[]>(productList);
 	const [trendingProducts] = useState<typeListProducts[]>(productList);
 	const [topSellingProducts] = useState<typeListProducts[]>(productList);
 
+	async function getProduct() {
+		const res = await fetch('https://localhost:7132/api/Products');
+		const data = await res.json();
+	  
+		setListProducts0(data)
+		return data
+	  }
+
+	  
 	return (
 		<main className='text-neutral-400 flex flex-col items-center z-0' >
-			<MainShowedProducts listProducts={listProducts}/>
+			<button className='w-24 h-24 bg-red-600' onClick={getProduct}> TESTE </button>
+			<button className='w-24 h-24 bg-green-600' onClick={() => console.log(listProducts0)}> SHOW </button>
+			<MainShowedProducts listProducts={listProducts0 ? listProducts0 : listProducts}/>
 			<DealsAndOutlet dealsProducts={dealsProducts}/>			
-			<article className='customContainer flex flex-col items-center '>
+			<article className='customContainer flex flex-col items-center '>				
 				<Sponsor />
 				<TrendingProducts trendingProducts={trendingProducts}/>			
 				<TopSellingProducts topSellingProducts={topSellingProducts}/>			
