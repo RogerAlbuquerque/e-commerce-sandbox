068ce94b1f19d4aaf5511c128d6992476195a64c
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2025-03-14 19:56:02 -0300

    chore:removed some unused comments

--- backend/C#/Ecommerce.Api/Program.cs
+++ backend/C#/Ecommerce.Api/Program.cs
@@ -25,11 +25,14 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
-
 var configuration = builder.Configuration;
-var connectionString = configuration.GetConnectionString("Mysql");
+string connectionstr = configuration.GetConnectionString("Mysql")!
+            .Replace("${DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST"))
+            .Replace("${DB_PORT}", Environment.GetEnvironmentVariable("DB_PORT"))
+            .Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME"))
+            .Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER"))
+            .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD")) ?? throw new Exception("The connection string for MySQL was not found.");
 
-Console.WriteLine($"String de conexão: {connectionString}");
 
 app.UseCors("AllowAll");
 
