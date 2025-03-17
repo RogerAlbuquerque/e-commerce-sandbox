4ca82bb1fd2e597d806ae2c9b245caf73a8a492c
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2025-03-17 02:51:25 -0300

    asdf

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import DealsAndOutlet from './Components/DealsAndOutlet';
 import Sponsor from './Components/Sponsors';
 import TrendingProducts from './Components/TrendingProducts';
@@ -10,40 +10,19 @@ import MainShowedProducts from './Components/MainShowedProducts';
 import { typeListProducts } from '../../@types/listProducts';
 
 
-
-
 export default function Index() {
 	const [productList, setProductList] = useState<typeListProducts[]>([])
 
+	useEffect(() => {
+		const API_URL = typeof window !== "undefined"
+			? process.env.NEXT_PUBLIC_API_URL
+			: process.env.INTERNAL_API_URL;
 
+		fetch(`${API_URL}/api/Products`)
+			.then(data => data.json())
+			.then(dt => setProductList(dt))
 
-		useEffect(() => {
-			const API_URL = typeof window !== "undefined"
-			? process.env.NEXT_PUBLIC_API_URL  // Para o navegador
-			: process.env.INTERNAL_API_URL;    // Para o SSR dentro do Docker
-
-			fetch(`${API_URL}/api/Products`)
-				.then(data => data.json())
-				.then(dt => setProductList(dt))
-	
-		}, []);
-
-	// async function callBackend() {
-	// 	const API_URL = typeof window !== "undefined"
-	// 		? process.env.NEXT_PUBLIC_API_URL  // Para o navegador
-	// 		: process.env.INTERNAL_API_URL;    // Para o SSR dentro do Docker
-
-	// 	try {
-	// 		const res = await fetch(`${API_URL}/api/Products`);
-	// 		const data = await res.json();
-	// 		setProductList(data)
-	// 	}
-	// 	catch {
-	// 		throw new Error("O BACKEND NÃO CHAMOU DIREITO AQUI ENTÃO DEU ERROOOOOOOOOOOOOOOOOOOOOOOOO----------------" + process.env.NEXT_PUBLIC_API_URL + '--------------------' + process.env.INTERNAL_API_URL);
-	// 	}
-	// }
-
-	// callBackend()
+	}, []);
 
 
 	if (productList.length === 0) {
