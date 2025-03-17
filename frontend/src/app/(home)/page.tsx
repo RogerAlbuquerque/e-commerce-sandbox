9427ffe90917833f7c90ddf0fcaeb6f20c506574
RogerAlbuquerque <rogeralbuquerque98@protonmail.com>
2025-03-17 01:24:13 -0300

    feat: try to use client components to better respond with backend on docker container

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client'
+import React,{useEffect, useState} from 'react';
 import DealsAndOutlet from './Components/DealsAndOutlet';
 import Sponsor from './Components/Sponsors';
 import TrendingProducts from './Components/TrendingProducts';
@@ -11,19 +12,43 @@ import { typeListProducts } from '../../@types/listProducts';
 
 
 
-export default async function Index() {
-	let productList:typeListProducts[] = [];
+export default function Index() {
+	const [productList, setProductList] = useState<typeListProducts[]>([])
+
+
+
+		useEffect(() => {
+			const API_URL = typeof window !== "undefined"
+			? process.env.NEXT_PUBLIC_API_URL  // Para o navegador
+			: process.env.INTERNAL_API_URL;    // Para o SSR dentro do Docker
+
+			fetch(`${API_URL}/api/Products`)
+				.then(data => data.json())
+				.then(dt => setProductList(dt))
 	
-	try {
-		const res = await fetch('http://localhost:8080/api/Products');
-		productList = await res.json();
-	}
-	catch {
-		productList = [];
-	}
+		}, []);
+
+	// async function callBackend() {
+	// 	const API_URL = typeof window !== "undefined"
+	// 		? process.env.NEXT_PUBLIC_API_URL  // Para o navegador
+	// 		: process.env.INTERNAL_API_URL;    // Para o SSR dentro do Docker
+
+	// 	try {
+	// 		const res = await fetch(`${API_URL}/api/Products`);
+	// 		const data = await res.json();
+	// 		setProductList(data)
+	// 	}
+	// 	catch {
+	// 		throw new Error("O BACKEND NÃO CHAMOU DIREITO AQUI ENTÃO DEU ERROOOOOOOOOOOOOOOOOOOOOOOOO----------------" + process.env.NEXT_PUBLIC_API_URL + '--------------------' + process.env.INTERNAL_API_URL);
+	// 	}
+	// }
+
+	// callBackend()
 
 
-	return (
+	if (productList.length === 0) {
+		return null
+	} else return (
 		<main className='text-neutral-400 flex flex-col items-center z-0' >
 			<MainShowedProducts listProducts={productList} />
 			<DealsAndOutlet dealsProducts={productList} />
