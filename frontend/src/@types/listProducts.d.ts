9fab8f183058cae7db2372fdd11852221850e155
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2025-04-20 10:21:18 -0300

    fix: Fix typescript whtih new property for color of products

--- frontend/src/@types/listProducts.d.ts
+++ frontend/src/@types/listProducts.d.ts
@@ -2,7 +2,7 @@ export interface typeListProducts {
 	productId: string,
 	name: string;
 	price: number;
-	color: string[] | ['#ffff', '#f1f1f1'];
+	hexColor: string[] | ['#ffff', '#f1f1f1'];
 	size?: string;
 	productCategories: string[];
 	featuredImagePath: string;
