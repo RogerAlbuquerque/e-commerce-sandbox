deae418d7819c8413d4a98cfd3d197d2f8da7d4a
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2026-01-02 11:23:01 -0300

    fix:update names of some tables

--- backend/Csharp/Ecommerce.Api/Program.cs
+++ backend/Csharp/Ecommerce.Api/Program.cs
@@ -5,14 +5,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
+	{
+	options.AddPolicy("AllowAll", policy =>
+		{
+		policy.AllowAnyOrigin()
+		.AllowAnyMethod()
+		.AllowAnyHeader();
+		});
+	});
 
 builder.Services.AddInfrastructureAPI(builder.Configuration);
 builder.Services.AddOpenApi();
@@ -27,11 +27,11 @@ if (app.Environment.IsDevelopment())
 }
 var configuration = builder.Configuration;
 string connectionstr = configuration.GetConnectionString("Mysql")!
-            .Replace("${DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST"))
-            .Replace("${DB_PORT}", Environment.GetEnvironmentVariable("DB_PORT"))
-            .Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME"))
-            .Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER"))
-            .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD")) ?? throw new Exception("The connection string for MySQL was not found.");
+.Replace("${DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST"))
+.Replace("${DB_PORT}", Environment.GetEnvironmentVariable("DB_PORT"))
+.Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME"))
+.Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER"))
+.Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD")) ?? throw new Exception("The connection string for MySQL was not found.");
 
 
 app.UseCors("AllowAll");
---
+++ backend/Csharp/Ecommerce.Infrastructure/Migrations/20251230003209_createDatabase.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ecommerce.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class createDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categories", x => x.CategoryId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "productstates",
+                columns: table => new
+                {
+                    ProductStateId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Sale = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    NewProduct = table.Column<bool>(type: "tinyint(1)", nullable: false),
+                    TopProduct = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_productstates", x => x.ProductStateId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    HexColor = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Size = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Stars = table.Column<byte>(type: "tinyint unsigned", nullable: false),
+                    FeaturedImagePath = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    SecondaryImagesPath = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    RegisterDate = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ProductStateId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.ProductId);
+                    table.ForeignKey(
+                        name: "FK_products_productstates_ProductStateId",
+                        column: x => x.ProductStateId,
+                        principalTable: "productstates",
+                        principalColumn: "ProductStateId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "categoriesproducts",
+                columns: table => new
+                {
+                    CategoryId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    ProductId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    CreatedAt = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categoriesproducts", x => new { x.ProductId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_categoriesproducts_categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_categoriesproducts_products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_categoriesproducts_CategoryId",
+                table: "categoriesproducts",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_products_ProductStateId",
+                table: "products",
+                column: "ProductStateId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categoriesproducts");
+
+            migrationBuilder.DropTable(
+                name: "categories");
+
+            migrationBuilder.DropTable(
+                name: "products");
+
+            migrationBuilder.DropTable(
+                name: "productstates");
+        }
+    }
+}
