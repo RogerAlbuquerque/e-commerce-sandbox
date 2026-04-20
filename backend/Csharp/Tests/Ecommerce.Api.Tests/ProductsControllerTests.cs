fb035c009cfe0d480b7399027cb1e51fef5d6fda
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2026-04-20 09:39:01 -0300

    test: Created test for post, put and delete methods of productsController

--- backend/Csharp/Tests/Ecommerce.Api.Tests/ProductsControllerTests.cs
+++ backend/Csharp/Tests/Ecommerce.Api.Tests/ProductsControllerTests.cs
@@ -2,6 +2,7 @@ using Ecommerce.Api.Controllers;
 using Ecommerce.Api.Tests.Builders;
 using Ecommerce.Application.DTOs;
 using Ecommerce.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -50,5 +51,111 @@ public class ProductsControllerTests
         var response = await controller.GetProductsById(id);
 
         Assert.IsType<NotFoundResult>(response.Result);
+    }
+
+    [Fact]
+    public async Task CreatNewProduct_returns_BadRequest_when_model_invalid()
+    {
+        var mock = new Mock<IProductService>();
+        var controller = new ProductsController(mock.Object);
+        controller.ModelState.AddModelError("Name", "invalid");
+
+        var dto = new ProductDtoBuilder().Build();
+        var response = await controller.CreatNewProduct(dto);
+
+        Assert.IsType<BadRequestObjectResult>(response);
+        mock.Verify(s => s.Add(It.IsAny<ProductDTO>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task CreatNewProduct_returns_CreatedAtRoute_when_valid()
+    {
+        var mock = new Mock<IProductService>();
+        var controller = new ProductsController(mock.Object);
+        var id = Guid.NewGuid();
+        var dto = new ProductDtoBuilder().WithProductId(id).Build();
+
+        var response = await controller.CreatNewProduct(dto);
+
+        var created = Assert.IsType<CreatedAtRouteResult>(response);
+        Assert.Equal(StatusCodes.Status201Created, created.StatusCode);
+        mock.Verify(s => s.Add(dto), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateOneProduct_returns_BadRequest_when_ids_differ()
+    {
+        var mock = new Mock<IProductService>();
+        var controller = new ProductsController(mock.Object);
+        var routeId = Guid.NewGuid();
+        var dto = new ProductDtoBuilder().WithProductId(Guid.NewGuid()).Build();
+
+        var response = await controller.UpdateOneProduct(routeId, dto);
+
+        Assert.IsType<BadRequestResult>(response);
+        mock.Verify(s => s.Update(It.IsAny<ProductDTO>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task UpdateOneProduct_returns_Ok_when_success()
+    {
+        var mock = new Mock<IProductService>();
+        var controller = new ProductsController(mock.Object);
+        var id = Guid.NewGuid();
+        var dto = new ProductDtoBuilder().WithProductId(id).WithName("abcd").WithPrice(3m).Build();
+
+        var response = await controller.UpdateOneProduct(id, dto);
+
+        var ok = Assert.IsType<OkObjectResult>(response);
+        Assert.Same(dto, ok.Value);
+        mock.Verify(s => s.Update(dto), Times.Once);
+    }
+
+
+    [Fact]
+    public async Task UpdateOneProduct_returns_NotFound_when_service_throws()
+    {
+        var mock = new Mock<IProductService>();
+        mock.Setup(s => s.Update(It.IsAny<ProductDTO>())).ThrowsAsync(new KeyNotFoundException());
+
+        var controller = new ProductsController(mock.Object);
+        var id = Guid.NewGuid();
+        var dto = new ProductDtoBuilder().WithProductId(id).Build();
+
+        var response = await controller.UpdateOneProduct(id, dto);
+
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task Delete_returns_NotFound_when_product_missing()
+    {
+        var id = Guid.NewGuid();
+        var mock = new Mock<IProductService>();
+        mock.Setup(s => s.GetById(id)).ThrowsAsync(new KeyNotFoundException());
+
+        var controller = new ProductsController(mock.Object);
+        var response = await controller.Delete(id);
+
+        Assert.IsType<NotFoundResult>(response.Result);
+        mock.Verify(s => s.Remove(It.IsAny<Guid?>()), Times.Never);
+    }
+
+
+    [Fact]
+    public async Task Delete_returns_Ok_when_removed()
+    {
+        var id = Guid.NewGuid();
+        var dto = new ProductDtoBuilder().WithProductId(id).Build();
+
+        var mock = new Mock<IProductService>();
+        mock.Setup(s => s.GetById(id)).ReturnsAsync(dto);
+
+        var controller = new ProductsController(mock.Object);
+        var response = await controller.Delete(id);
+
+        var ok = Assert.IsType<OkObjectResult>(response.Result);
+        Assert.Same(dto, ok.Value);
+        mock.Verify(s => s.Remove(id), Times.Once);
     }
 }
