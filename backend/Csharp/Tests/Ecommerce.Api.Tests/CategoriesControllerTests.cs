2833a9424143b95a2da7ff110bf7280e71de3bae
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2026-04-22 08:09:21 -0300

    test: added test to Get methods for categories

--- backend/Csharp/Tests/Ecommerce.Api.Tests/CategoriesControllerTests.cs
+++ backend/Csharp/Tests/Ecommerce.Api.Tests/CategoriesControllerTests.cs
@@ -1,9 +1,55 @@
+using Ecommerce.Api.Controllers;
+using Ecommerce.Api.Tests.Builders;
+using Ecommerce.Application.DTOs;
+using Ecommerce.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
 namespace Ecommerce.Api.Tests;
 public class categoriesControllerTests
 {
     [Fact]
     public async Task GetCategories_returns_Ok_with_payload()
     {
+        var list = new List<CategoryDTO> { new CategoryDtoBuilder().Build() };
+        var mock = new Mock<ICategoryService>();
+        mock.Setup(s => s.GetAllCategories()).ReturnsAsync(list);
 
+        var controller = new CategoriesController(mock.Object);
+        var response = await controller.GetCategories();
+
+        var ok = Assert.IsType<OkObjectResult>(response.Result);
+        Assert.Same(list, ok.Value);
     }
+
+    [Fact]
+    public async Task GetCategoryById_returns_Ok_when_found()
+    {
+        var id = Guid.NewGuid();
+        var dto = new CategoryDtoBuilder().WithCategoryId(id).Build();
+
+        var mock = new Mock<ICategoryService>();
+        mock.Setup(s => s.GetById(id)).ReturnsAsync(dto);
+
+        var controller = new CategoriesController(mock.Object);
+        var response = await controller.GetCategoryById(id);
+
+        var ok = Assert.IsType<OkObjectResult>(response.Result);
+        Assert.Same(dto, ok.Value);
+    }
+
+    [Fact]
+    public async Task GetCategoryById_returns_NotFound_when_missing()
+    {
+        var id = Guid.NewGuid();
+        var mock = new Mock<ICategoryService>();
+        mock.Setup(s => s.GetById(id)).ThrowsAsync(new KeyNotFoundException());
+
+        var controller = new CategoriesController(mock.Object);
+        var response = await controller.GetCategoryById(id);
+
+        Assert.IsType<NotFoundResult>(response.Result);
+    }
+
 }
