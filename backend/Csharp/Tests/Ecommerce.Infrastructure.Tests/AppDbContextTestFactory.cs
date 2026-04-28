198b2d62c1af74805fcab5ecfcdde33c589047fa
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2026-04-28 08:33:28 -0300

    style:forgot some comments

--- backend/Csharp/Tests/Ecommerce.Infrastructure.Tests/AppDbContextTestFactory.cs
+++ backend/Csharp/Tests/Ecommerce.Infrastructure.Tests/AppDbContextTestFactory.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ecommerce.Infrastructure.Tests
+﻿namespace Ecommerce.Infrastructure.Tests
 {
     internal class AppDbContextTestFactory
     {
