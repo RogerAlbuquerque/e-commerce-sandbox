4fca8cec6c86876a2f138487255b6c9cab2b775b
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2026-04-30 12:31:57 -0300

    feat/test: Added TestFactory to appDbContexto with InMemory package

--- backend/Csharp/Tests/Ecommerce.Infrastructure.Tests/AppDbContextTestFactory.cs
+++ backend/Csharp/Tests/Ecommerce.Infrastructure.Tests/AppDbContextTestFactory.cs
@@ -1,6 +1,17 @@
-﻿namespace Ecommerce.Infrastructure.Tests
+﻿using Ecommerce.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Infrastructure.Tests;
+
+internal static class AppDbContextTestFactory
 {
-    internal class AppDbContextTestFactory
+    public static AppDbContext CreateInMemoryContext(string? databaseName = null)
     {
+        var name = databaseName ?? Guid.NewGuid().ToString();
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(name)
+            .Options;
+
+        return new AppDbContext(options);
     }
 }
