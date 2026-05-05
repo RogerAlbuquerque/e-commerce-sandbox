890cbfee754377717ec5efaedd15134c3734adcc
Roger Albuquerque <rogeralbuquerque98@protonmail.com>
2026-05-05 10:01:39 -0300

    feat/test: Created all test to infrastructure repositories

--- backend/Csharp/Tests/Ecommerce.Infrastructure.Tests/CategoryRepositoryTests.cs
+++ backend/Csharp/Tests/Ecommerce.Infrastructure.Tests/CategoryRepositoryTests.cs
@@ -45,4 +45,60 @@ public class CategoryRepositoryTests
 
         await Assert.ThrowsAsync<KeyNotFoundException>(() => sut.GetByIdAsync(Guid.NewGuid()));
     }
+
+
+    [Fact]
+    public async Task CreateAsync_persists_category()
+    {
+        await using var ctx = AppDbContextTestFactory.CreateInMemoryContext();
+        var sut = new CategoryRepository(ctx);
+        var cat = new CategoryBuilder().WithName("x").Build();
+
+        await sut.CreateAsync(cat);
+
+        Assert.Single(ctx.Categories);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_persists_changes()
+    {
+        const string sharedDb = "category-repo-update";
+        var id = Guid.NewGuid();
+
+        await using (var ctx = AppDbContextTestFactory.CreateInMemoryContext(sharedDb))
+        {
+            var cat = new CategoryBuilder().WithCategoryId(id).WithName("old").Build();
+            ctx.Categories.Add(cat);
+            await ctx.SaveChangesAsync();
+        }
+
+        await using (var ctx = AppDbContextTestFactory.CreateInMemoryContext(sharedDb))
+        {
+            var updated = new CategoryBuilder().WithCategoryId(id).WithName("new").Build();
+            var sut = new CategoryRepository(ctx);
+            await sut.UpdateAsync(updated);
+        }
+
+        await using (var ctx = AppDbContextTestFactory.CreateInMemoryContext(sharedDb))
+        {
+            var sut = new CategoryRepository(ctx);
+            var again = await sut.GetByIdAsync(id);
+            Assert.Equal("new", again.Name);
+        }
+    }
+
+    [Fact]
+    public async Task RemoveAsync_deletes_category()
+    {
+        var id = Guid.NewGuid();
+        await using var ctx = AppDbContextTestFactory.CreateInMemoryContext();
+        var cat = new CategoryBuilder().WithCategoryId(id).WithName("z").Build();
+        ctx.Categories.Add(cat);
+        await ctx.SaveChangesAsync();
+
+        var sut = new CategoryRepository(ctx);
+        await sut.RemoveAsync(cat);
+
+        Assert.Empty(ctx.Categories);
+    }
 }
